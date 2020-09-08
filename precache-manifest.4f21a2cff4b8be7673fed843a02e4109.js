bdefa8989e4a0b9f91d7551c3717a1836ba58030
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-09-08 16:50:03 +0530

    Updates

---
+++ precache-manifest.4f21a2cff4b8be7673fed843a02e4109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b335fd505e86444422c4e38e079d2380",
+    "url": "/slot-matching-app/index.html"
+  },
+  {
+    "revision": "dd5c7c41d3f5160c30ed",
+    "url": "/slot-matching-app/static/css/main.e3b2d93a.chunk.css"
+  },
+  {
+    "revision": "8a6c017237d0c304b844",
+    "url": "/slot-matching-app/static/js/2.52654b68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slot-matching-app/static/js/2.52654b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5c7c41d3f5160c30ed",
+    "url": "/slot-matching-app/static/js/main.ac098d96.chunk.js"
+  },
+  {
+    "revision": "5e0d0ea95ec096bdfd57",
+    "url": "/slot-matching-app/static/js/runtime-main.219f948e.js"
+  }
+]);
